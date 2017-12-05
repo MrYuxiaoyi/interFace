5783752e583e7087b2673a838c8b175cd464aa76
YUXIAOYI <695544867@qq.com>
2017-12-05 09:54:22 +0800

    初始化项目

---
+++ Interface/Interface/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Interface
+//
+//  Created by YXY on 2017/12/5.
+//  Copyright © 2017年 XiaoYuEr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
